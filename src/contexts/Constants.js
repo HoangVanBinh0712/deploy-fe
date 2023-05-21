7543028811e23f4ff5040eb3e3b99da019417455
HoangVanBinh0712 <thebest11447@gmail.com>
2023-05-21 15:27:21 +0700

    ...

--- src/contexts/Constants.js
+++ src/contexts/Constants.js
@@ -1,6 +1,7 @@
-export const apiUrl = 'http://localhost:8080/api'
-export const apiWS = 'localhost:8080'
+export const apiUrl = process.env.APIURL;
+export const apiWS = process.env.APIWS;
 /* export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:8080/api' : 'https://career-website.herokuapp.com/api' */
-export const LOCAL_STORAGE_TOKEN_NAME = 'user-token'
-export const USER_ROLE = 'USER_ROLE'
-export const webUrlActivity = '/user/home /admin/dashboard /admin/account /admin/post /admin/services /admin/form /admin/bar /admin/pie /admin/line /admin/industries /admin/reports /admin/form /user/login /employer/login /user/login /admin/login '
+export const LOCAL_STORAGE_TOKEN_NAME = "user-token";
+export const USER_ROLE = "USER_ROLE";
+export const webUrlActivity =
+  "/user/home /admin/dashboard /admin/account /admin/post /admin/services /admin/form /admin/bar /admin/pie /admin/line /admin/industries /admin/reports /admin/form /user/login /employer/login /user/login /admin/login ";
